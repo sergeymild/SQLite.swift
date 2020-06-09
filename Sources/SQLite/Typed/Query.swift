69406208a2ca0027b31aea1ab8f8251e0d8faf10
sergeymild <sergeymild@yandex.ru>
2020-06-09 14:09:08 +0300

    #811

--- Sources/SQLite/Typed/Query.swift
+++ Sources/SQLite/Typed/Query.swift
@@ -1003,7 +1003,6 @@ extension Connection {
                             try expandGlob(true)(q)
                             continue column
                         }
-                        throw QueryError.noSuchTable(name: namespace)
                     }
                     throw QueryError.noSuchTable(name: namespace)
                 }
